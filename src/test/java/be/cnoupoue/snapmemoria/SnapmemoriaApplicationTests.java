8a77cb2c60e47fd4eddf758ecc45b03e0e331a01
Cameron <cameronnoupoue@gmail.com>
2026-07-01 10:13:45 +0200

    chore: initialize Spring Boot project

---
+++ src/test/java/be/cnoupoue/snapmemoria/SnapmemoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.cnoupoue.snapmemoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapmemoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
